7bcf49bc2598676216c79a56767a8789bd130e55
Ko2m0 <kozmopimp@gmail.com>
2022-10-13 17:45:40 -0500

    css trix w --var

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if ($mysqli -> connect_errno) {
     exit();
   }
   
-  $sql = "SELECT * FROM connextus_demo_data WHERE user_id='6'";
+  $sql = "SELECT * FROM connextus_demo_data WHERE user_id='1'";
   $result = $mysqli -> query($sql);
   
   // Associative array
@@ -566,6 +566,19 @@ if ($mysqli -> connect_errno) {
                 modalInfo.style.display = "none";
             }
         }
+    </script>
+    <script>
+        // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
     </script>
 
 </body>
